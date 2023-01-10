b88e56025a28dfebd47528b17c50339e25e430c3
lokakshi <adityalokakshijee@gmail.com>
2023-01-10 15:24:57 +0530

    First java program

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args){
-        Scanner input=new Scanner(System.in);
-        String name= input.next();
-        System.out.println(name);
+
+
+        System.out.println(
+                "Hello world,First java program"
+        );
     }
 }
